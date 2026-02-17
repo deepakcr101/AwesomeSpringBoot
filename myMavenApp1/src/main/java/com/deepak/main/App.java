c364d9fce5cd71c056604534a1777c562089a7d1
deepak <piomega01@gmail.com>
2026-02-17 22:25:42 +0530

    project restructure

--- myMavenApp1/src/main/java/com/deepak/main/App.java
+++ myMavenApp1/src/main/java/com/deepak/main/App.java
@@ -10,6 +10,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello My First Maven Project");
 		String config_file_pathString = "/com/deepak/resources/applicationContext.xml";
+		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext(config_file_pathString);
 
 		Student std1 = (Student) context.getBean(Student.class);
